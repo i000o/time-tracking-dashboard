fbf4c5a9b28b15eb8fef3a50cf2f7337339fdcbb
i000o <iosman1096@gmail.com>
2026-08-02 15:09:11 +0100

    dynamic value update logic implemented

--- src/main.js
+++ src/main.js
@@ -1,60 +1,47 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+let hours; 
 
-<div class="ticks"></div>
+async function fetchData() { 
+    const response = await fetch('./data.json');
+    hours = await response.json(); 
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+    showHours(hours); 
+}
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+fetchData(); 
 
-setupCounter(document.querySelector('#counter'))
+const buttons = document.querySelectorAll("button[type='button']"); 
+const cards = document.querySelectorAll(".card");
+
+function updateDisplay(event) { 
+    const clickedButton = event.currentTarget; 
+    const period = clickedButton.dataset.path; // stores selected button 
+
+    // loop through the cards (work, social etc.) and inject data accordingly 
+    cards.forEach((card) => { 
+        const categoryTitle = card.dataset.path; 
+
+        // find the concurrent data 
+        const category = hours.find((item) => { 
+            return item.title === categoryTitle; 
+        }); 
+
+        const currentValue = category.timeframes[period].current; 
+        const previousValue = category.timeframes[period].previous; 
+
+        // find this card's spans and update 
+        const currentSpan = card.querySelector(".current-hours"); 
+        currentSpan.textContent = currentValue; 
+        currentSpan.style.display = 'block'; 
+
+        const previousSpan = card.querySelector(".previous-hours"); 
+        previousSpan.textContent = previousValue; 
+        previousSpan.style.display = 'block'; 
+    });
+}
+
+buttons.forEach((button) => { 
+    button.addEventListener("click", updateDisplay);
+});
+
